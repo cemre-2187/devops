b4f55e309ecc96749a2d5f6531458e34545c1bf4
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2023-06-16 19:45:25 +0300

    add service

--- addService.js
+++ addService.js
@@ -1,11 +1,17 @@
 const addService = (numberOne, numberTwo) => {
-  if (!numberOne || !numberTwo) {
-    return { message: "Number can not be empty" }
+  // if (!numberOne || !numberTwo) {
+  //   return { message: "Number can not be empty" }
+  // }
+
+  if(numberOne === "0" || numberTwo === "0"){
+    return { message: "Numbers can not be zero" }
   }
+
+
   const sum = parseInt(numberOne) + parseInt(numberTwo);
 
-  // This Feature will be added in the test
-  // let sumDecimal = Number(numberOne) + Number(numberTwo);
+  // This Feature will be added in the test 
+  // let sum = Number(numberOne) + Number(numberTwo);
 
   // This will be hotfix
   // if (numberOne < 0 || numberTwo < 0) {
